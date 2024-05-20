2467ff61f5b8cec18ab32644812f530c7fe8b459
Animesh Mondal <animeshmondal1414@gmail.com>
2024-05-20 19:36:46 +0530

    first commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-slate-200 text-center text-sm tracking-wide p-5 w-full">
+      &copy;Animesh Mondal 2024
+    </footer>
+  );
+}
+
+export default Footer;
